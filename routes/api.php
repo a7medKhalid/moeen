66e9044bd9cf879136368fadfde782b78c061340
a7medKhalid <ahmed1alghamdi2000@gmail.com>
2024-06-01 02:30:22 +0300

    auth routes

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,27 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::post('/suggestions', function (Request $request){
+    $request->validate([
+        'name' => 'required|string',
+        'email' => 'nullable|email',
+        'title' => 'required|string',
+        'content' => 'required|string'
+    ]);
+
+    $suggestion = \App\Models\Core\Suggestion::create($request->all());
+
+    return response()->json($suggestion, 201);
+});
+
+
+Route::prefix('monset')->group(function () {
+    include 'Monset/api.php';
+});
+
+
+Route::prefix('auth')->group(function () {
+    include 'auth.php';
+});
