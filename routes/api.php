3bb5dc3cea8b7c0da8a15111e1dbcc7d47db6aaf
a7medKhalid <ahmed1alghamdi2000@gmail.com>
2024-06-01 02:21:24 +0300

    breeze installed

--- routes/api.php
+++ routes/api.php
@@ -3,25 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
-
-
-Route::post('/suggestions', function (Request $request){
-    $request->validate([
-        'name' => 'required|string',
-        'email' => 'nullable|email',
-        'title' => 'required|string',
-        'content' => 'required|string'
-    ]);
-
-    $suggestion = \App\Models\Core\Suggestion::create($request->all());
-
-    return response()->json($suggestion, 201);
-});
-
-
-Route::prefix('monset')->group(function () {
-    include 'Monset/api.php';
 });
