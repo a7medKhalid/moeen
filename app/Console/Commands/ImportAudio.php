93edb2bf4d47421c9eeb4e455c5c7a7f7a517bad
a7medKhalid <ahmed1alghamdi2000@gmail.com>
2024-06-20 03:28:27 +0300

    idk

--- app/Console/Commands/ImportAudio.php
+++ app/Console/Commands/ImportAudio.php
@@ -32,52 +32,54 @@ class ImportAudio extends Command
 
     public function getAudioFiles()
     {
-        for ($i = 1; $i < 115; $i++) {
-            $url = 'https://api.qurancdn.com/api/qdc/audio/reciters/7/audio_files?chapter=' . $i .'&segments=true';
+        for ($r = 2; $r < 175; $r++) {
+            for ($c = 2; $c < 115; $c++) {
+                $url = 'https://api.qurancdn.com/api/qdc/audio/reciters/' . $r . '/audio_files?chapter=' . $c . '&segments=true';
 
-            try {
-                $response = Http::get($url);
+                try {
+                    $response = Http::get($url);
 
-                if ($response->successful()) {
-                    // If the response is successful, you can return the data or process it as needed
-                    $data = $response->json()['audio_files'][0];
-                    $audio_url = $data['audio_url'];
-                    $verse_timings = $data['verse_timings'];
+                    if ($response->successful()) {
+                        // If the response is successful, you can return the data or process it as needed
+                        $data = $response->json()['audio_files'][0];
+                        $audio_url = $data['audio_url'];
+                        $verse_timings = $data['verse_timings'];
 
 
-                    $reciter = \App\Models\Monset\Reciter::firstOrCreate([
-                        'name' => 'mshary'
-                    ]);
-
-                    $surah = \App\Models\Core\Surah::firstOrCreate([
-                        'name' => $i,
-                        'order' => $i
-                    ]);
-
-                    $audioFile = AudioFile::firstOrCreate([
-                        'url' => $audio_url,
-                        'reciter_id' => $reciter->id
-                    ]);
-
-                    $counter = 0;
-                    foreach ($verse_timings as $timing) {
-                        $counter++;
-
-                        $verse = \App\Models\Core\Verse::firstOrCreate([
-                            'order' => $counter,
-                            'surah_id' => $surah->id
+                        $reciter = \App\Models\Monset\Reciter::firstOrCreate([
+                            'name' => $r
                         ]);
 
-                        $audioFile->segments()->firstOrCreate([
-                            'start_time' => $timing['timestamp_from'],
-                            'end_time' => $timing['timestamp_to'],
-                            'type' => \App\Enums\Monset\Segment\Type::verse,
-                            'type_id' => $verse->id
+                        $surah = \App\Models\Core\Surah::firstOrCreate([
+                            'name' => $c,
+                            'order' => $c
                         ]);
+
+                        $audioFile = AudioFile::firstOrCreate([
+                            'url' => $audio_url,
+                            'reciter_id' => $reciter->id
+                        ]);
+
+                        $counter = 0;
+                        foreach ($verse_timings as $timing) {
+                            $counter++;
+
+                            $verse = \App\Models\Core\Verse::firstOrCreate([
+                                'order' => $counter,
+                                'surah_id' => $surah->id
+                            ]);
+
+                            $audioFile->segments()->firstOrCreate([
+                                'start_time' => $timing['timestamp_from'],
+                                'end_time' => $timing['timestamp_to'],
+                                'type' => \App\Enums\Monset\Segment\Type::verse,
+                                'type_id' => $verse->id
+                            ]);
+                        }
+
                     }
-
+                } catch (\Exception $e) {
                 }
-            } catch (\Exception $e) {
             }
         }
     }
