3071db0cb9dcf1a4238a0dd5a38df4f7d8b97add
a7medKhalid <ahmed1alghamdi2000@gmail.com>
2024-08-11 18:42:38 +0300

    done

--- app/Http/Controllers/Monset/SearchController.php
+++ app/Http/Controllers/Monset/SearchController.php
@@ -3,14 +3,30 @@
 namespace App\Http\Controllers\Monset;
 
 use App\Http\Controllers\Controller;
+use App\Models\Core\Verse;
+use App\Models\Monset\Clip;
 use Illuminate\Http\Request;
 
 class SearchController extends Controller
 {
-//    public function search(){
+    public function search(){
 //        $versesIds = request()->get('versesIds');
 //        $search = request()->get('search');
-//
-//        foreach ($verses as
-//    }
+        $stringVersesIds = ['1:2', '2:3', '3:4'];
+        $search = 'الفاتحة';
+
+        $searchClips = Clip::whereHas('phrases', function ($query) use ($search){
+            $query->where('value', 'like', "%$search%");
+        })->get();
+        $versesClips = [];
+        foreach ($stringVersesIds as $verse){
+            $verse = explode(':', $verse);
+            $chapter = $verse[0];
+            $verse = $verse[1];
+            $verses = Verse::where('chapter', $chapter)->where('verse', $verse)->first();
+            $verseClips = $verses->clips;
+            $versesClips = array_merge($versesClips, $verseClips->toArray());
+        }
+        return response()->json(array_merge($searchClips->toArray(), $versesClips));
+    }
 }
--- app/Models/Core/Verse.php
+++ app/Models/Core/Verse.php
@@ -2,9 +2,11 @@
 
 namespace App\Models\Core;
 
+use App\Models\Monset\Clip;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Verse extends Model
 {
@@ -12,5 +14,9 @@ class Verse extends Model
 
     protected function orderedId(): Attribute{
         return Attribute::get(fn() => $this->surah_id . ':' . $this->order);
+    }
+
+    public function clips(): BelongsToMany{
+        return $this->belongsToMany(Clip::class, 'clip_has_verses');
     }
 }
