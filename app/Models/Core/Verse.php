66dfff723c923fcdce9dbf6b3e8f02aaed659cb7
a7medKhalid <ahmed1alghamdi2000@gmail.com>
2024-08-11 18:20:10 +0300

    done

--- app/Models/Core/Verse.php
+++ app/Models/Core/Verse.php
@@ -2,10 +2,15 @@
 
 namespace App\Models\Core;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Verse extends Model
 {
     use HasFactory;
+
+    protected function orderedId(): Attribute{
+        return Attribute::get(fn() => $this->surah_id . ':' . $this->order);
+    }
 }
